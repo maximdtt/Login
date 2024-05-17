7902b0f9cc1d8ddd0ce8d5a4bf76978f10abbca8
Maksims Salajevs <maximdtt@gmail.com>
2024-05-17 17:57:44 +0100

    first commit

---
+++ LoginLo/SecondViewController.swift
@@ -0,0 +1,11 @@
+//
+//  SecondViewController.swift
+//  LoginLo
+//
+//  Created by Maksims Šalajevs on 17/05/2024.
+//
+
+import UIKit
+final class SecondViewController: UIViewController {
+    
+}
---
+++ LoginLo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LoginLo
+//
+//  Created by Maksims Šalajevs on 17/05/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
