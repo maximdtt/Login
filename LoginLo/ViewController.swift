ceb269e678d6a7fcc62f3c535267d4da1e295de5
Maksims Salajevs <maximdtt@gmail.com>
2024-05-18 20:28:51 +0100

    no message

--- LoginLo/ViewController.swift
+++ LoginLo/ViewController.swift
@@ -8,10 +8,42 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // MARK: - IBOutlets
+    @IBOutlet weak var textFieldEmail: UITextField!
+    @IBOutlet weak var textFieldPassword: UITextField!
+    @IBOutlet weak var emailViewLine: UIView!
+    @IBOutlet weak var passwordViewLine: UIView!
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var signUpButton: UIButton!
+    @IBOutlet weak var noAccountLabel: UILabel!
+    
 
+    
+    // MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        configureLoginButton()
+    }
+    
+    // MARK: - Actions
+    @IBAction func loginButtonAction(_ sender: UIButton) {
+        print("Login pressed")
+    }
+    @IBAction func signUpButtonAction(_ sender: UIButton) {
+        print("Signed up pressed")
+    }
+    
+    
+    
+    // MARK: - Methods
+    
+    func configureLoginButton() {
+        loginButton.layer.shadowColor = UIColor.green.cgColor
+        loginButton.layer.shadowOffset = CGSize.init(width: 2, height: 6)
+        loginButton.layer.shadowOpacity = 0.5
+        loginButton.layer.shadowRadius = 5
     }
 
 
